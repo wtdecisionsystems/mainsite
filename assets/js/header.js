1585d27220b64f61998b4fa04ee79b2a1c6e5453
WTDS Development <development@wtdecisionsystems.com>
2022-01-20 06:22:06 -0600

    New site

--- assets/js/header.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-  // this will work when your window scrolled.
-  var scroll = $(window).scrollTop(); //getting the scrolling height of window
-  if (scroll > 100) {
-    $('.header').addClass('header-scrolled');
-  } else {
-    $('.header').removeClass('header-scrolled');
-  }
-});
--- assets/js/pages/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('.slider').slick({
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: false,
-  });
-});
